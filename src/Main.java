897d709c661b89474e7bad5bbd88e333f6ac7514
AlexNchk <alexnataforever@gmail.com>
2022-08-30 18:58:44 +0300

    Fourth Commit Added new branch "lambda", delete file NumberWordsSurnameComparator, added lambda expressions

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -10,7 +11,15 @@ public class Main {
         people.add(new Person("Акиф", "Абдул оглы Абн", 15));
         people.add(new Person("Антонио", "Кампус Родригес", 35));
         people.add(new Person("Александр", "Панкратов Черный", 55));
-        people.sort(new NumberWordsSurnameComparator(3));
+        Comparator<Person> NumberWordsSurnameComparator = (Person o1, Person o2) ->{
+            String[] surname1 = o1.getSurname().split(" ");
+            String[] surname2 = o2.getSurname().split(" ");
+            if ((surname1.length >= 3 && surname2.length >= 3) || surname1.length == surname2.length) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+            return surname1.length > surname2.length ? 1 : -1;
+        };
+        people.sort(NumberWordsSurnameComparator);
         System.out.println(people);
     }
 }
